f228e7155708d86bf9b45c713b12f64a78e30f55
ValentinaRippe <lauravalentina200102@gmail.com>
2022-05-26 08:04:25 -0500

    Deploy api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,9 @@
 import axios from "axios"
 
 export default axios.create({
+  baseURL: "https://musicapp-apiv.herokuapp.com",
+});
+
+/* export default axios.create({
     baseURL: "http://localhost:4000",
-  });
+  }); */
